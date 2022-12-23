6d56d687471e469ae8cb47c0878ed1ebe6072046
stephenryan183 <stephenryan183@gmail.com>
2022-12-22 17:22:03 -0800

    updated-javascript

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-favoriteMovieGenre("regular")
-favoritefruit("regular")
-favoriteMode('light')
-favoriteEdgeStyle('sharp')
+/*this is an in line
+comment*/
+var myName = "Stephen";
+
+var myLastName = "Ryan";
+
+let myMiddleName ="John";
+
+var b =2;
+console.log(b);
+a = 9;
+
+b = a;
+
+console.log(b);
+
+
+
+//Initialixe these three variables
+
+var c = 5;
+var d = 10
+var e ="I am a ";
+
+c = c + 1;
+d = d + 5;
+e = e + "string!";
+
+console.log(c,d,e)
+
+
+// below will give us the sum of the two
+var sum = 10 + 10;
+console.log(sum)
+
+//below will give us the diffrence between the two
+
+var diffrence = 45 - 9;
+console.log(diffrence)
+
+//below will give us the multiplication product
+
+var product = 8 * 10;
+console.log(product)
+
+var quotient = 66 / 11;
+console.log(quotient)
+
+
+
+
+
